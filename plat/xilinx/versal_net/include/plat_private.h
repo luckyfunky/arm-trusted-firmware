1fbadf7b31ebcf86bf4224445ec969643bd0ca75
Jay Buddhabhatti <jay.buddhabhatti@xilinx.com>
2022-09-05 02:56:32 -0700

    feat(versal-net): add support for platform management
    Add support for PM EEMI interface for Versal_net. Also use PM
    APIs in psci ops. Added TFA_NO_PM flag to disable PM functionality.
    
    Signed-off-by: Jay Buddhabhatti <jay.buddhabhatti@xilinx.com>

--- plat/xilinx/versal_net/include/plat_private.h
+++ plat/xilinx/versal_net/include/plat_private.h
@@ -9,6 +9,12 @@
 #define PLAT_PRIVATE_H
 
 #include <lib/xlat_tables/xlat_tables_v2.h>
+#include <bl31/interrupt_mgmt.h>
+
+typedef struct versal_intr_info_type_el3 {
+	uint32_t id;
+	interrupt_type_handler_t handler;
+} versal_intr_info_type_el3_t;
 
 void versal_net_config_setup(void);
 
@@ -17,7 +23,12 @@ const mmap_region_t *plat_versal_net_get_mmap(void);
 void plat_versal_net_gic_driver_init(void);
 void plat_versal_net_gic_init(void);
 void plat_versal_net_gic_cpuif_enable(void);
+void plat_versal_net_gic_cpuif_disable(void);
 void plat_versal_net_gic_pcpu_init(void);
+void plat_versal_net_gic_save(void);
+void plat_versal_net_gic_resume(void);
+void plat_versal_net_gic_redistif_on(void);
+void plat_versal_net_gic_redistif_off(void);
 
 extern uint32_t cpu_clock, platform_id, platform_version;
 void board_detection(void);
@@ -25,6 +36,11 @@ char *board_name_decode(void);
 uint64_t smc_handler(uint32_t smc_fid, uint64_t x1, uint64_t x2, uint64_t x3,
 		       uint64_t x4, void *cookie, void *handle, uint64_t flags);
 int32_t sip_svc_setup_init(void);
+/*
+ * Register handler to specific GIC entrance
+ * for INTR_TYPE_EL3 type of interrupt
+ */
+int request_intr_type_el3(uint32_t irq, interrupt_type_handler_t fiq_handler);
 
 #define PM_IOCTL			34U
 #define PM_GET_CHIPID			24U
